e0691ad68c52f7064b344ef1506a3fd8b2634d8a
calbertora <calbertora@gmail.com>
2019-03-14 23:17:05 -0500

    First assignment done

--- Assignment Week 1/AcceptanceCriteria.js
+++ Assignment Week 1/AcceptanceCriteria.js
@@ -1,1 +1,43 @@
-console.log('initial');
+/**************************************************************************
+ * 
+ * Como interesado necesito obtener la información de cursos ofertados por educación continua 
+ * para así tener diferentes opciones para elegir posteriormente un curso al cual matricularme.
+ * 
+ **************************************************************************/
+let courses = [
+   {
+      id: 1,
+      name: 'Diseño web',
+      duration: 40,
+      price: null,
+   },
+   {
+      id: 2,
+      name: 'NodeJS',
+      duration: 32,
+      price: null,
+   },
+   {
+      id: 3,
+      name: 'Programación',
+      duration: 40,
+      price: 50000,
+   },
+];
+
+let timer = 0;
+function getCourseOffering() {
+
+   courses.forEach((item) => {
+      timer++;
+      printValue(item,timer); 
+   })
+};
+
+function printValue(item,timer){
+   setTimeout(() => {
+      console.log(item);
+   }, 2000*timer);
+}
+
+getCourseOffering();
